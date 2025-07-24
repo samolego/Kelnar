af57fb7f7da29b0badf2d5c47cb9ef46285142f9
samolego <34912839+samolego@users.noreply.github.com>
2025-07-24 23:06:53 +0200

    detect url instead of hardcoding it

--- composeApp/src/desktopMain/kotlin/io/github/samolego/kelnar/ui/screens/ProductsShareScreen.desktop.kt
+++ composeApp/src/desktopMain/kotlin/io/github/samolego/kelnar/ui/screens/ProductsShareScreen.desktop.kt
@@ -1,7 +1,6 @@
 package io.github.samolego.kelnar.ui.screens
 
 import java.net.URLEncoder
-import io.github.samolego.kelnar.utils.AppConfig
 
 actual fun encodeURIComponent(str: String): String {
     return URLEncoder.encode(str, "UTF-8")
--- composeApp/src/wasmJsMain/kotlin/io/github/samolego/kelnar/ui/screens/ProductsShareScreen.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/samolego/kelnar/ui/screens/ProductsShareScreen.wasmJs.kt
@@ -1,12 +1,11 @@
 package io.github.samolego.kelnar.ui.screens
 
 import kotlinx.browser.window
-import io.github.samolego.kelnar.utils.AppConfig
 
 actual fun encodeURIComponent(str: String): String {
     return io.github.samolego.kelnar.encodeURIComponent(str)
 }
 
 actual fun getCurrentBaseUrl(): String {
-    return AppConfig.BASE_URL
+    return "${window.location.protocol}//${window.location.host}${window.location.pathname}"
 }
