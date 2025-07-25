cd6cf342dce969b465eb9e79aad66958f4f9e96f
samolego <34912839+samolego@users.noreply.github.com>
2025-07-25 21:45:04 +0200

    fix android clipboard

--- composeApp/src/androidMain/kotlin/io/github/samolego/kelnar/utils/ClipboardUtils.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/samolego/kelnar/utils/ClipboardUtils.android.kt
@@ -5,16 +5,10 @@ import android.content.ClipboardManager
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import io.github.samolego.kelnar.AppContext
 
 actual fun copyToClipboard(text: String) {
-    // This will be called from a Composable context, so we need to get context differently
-    // For now, we'll provide a simpler implementation that can be enhanced later
-    println("Android clipboard: $text")
-}
-
-@Composable
-fun copyToClipboardAndroid(text: String) {
-    val context = LocalContext.current
+    val context = AppContext.get()
     val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
     val clipData = ClipData.newPlainText("Shared Products", text)
     clipboardManager.setPrimaryClip(clipData)
